f9a2617be76051c18996888af6c621b9bbea2845
Ricardo Fuentes Urbina <ricardo.furbina@alumnos.udg.mx>
2022-08-25 19:11:27 -0500

    detalles

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var button = document.querySelector('.share-button');
 var tooltip = document.querySelector('.tooltip');
+var shareButton = document.getElementById("shareButton");
 
 button.addEventListener('mouseover', (event) => {
     tooltip.style.opacity = 1;
+    button.style.backgroundColor = '#48556a';
+    shareButton.style.fill = "#ecf2f8";
 });
 
 button.addEventListener('mouseout', (event) => {
     tooltip.style.opacity = 0;
+    button.style.backgroundColor = '#f0f0f0';
+    shareButton.style.fill = "#6e8098";
 });
